a3d30974d17321b2182923bdff3ffd491e47903e
hyoJ <goodforx0147@naver.com>
2025-06-19 10:04:59 +0900

    style: 코드 정리

--- src/main/java/com/skala/decase/domain/requirement/service/ExelExportService.java
+++ src/main/java/com/skala/decase/domain/requirement/service/ExelExportService.java
@@ -5,29 +5,18 @@ import com.skala.decase.domain.project.controller.dto.response.MappingTableRespo
 import com.skala.decase.domain.requirement.controller.dto.response.MatrixResponse;
 import com.skala.decase.domain.requirement.controller.dto.response.RequirementWithSourceResponse;
 import com.skala.decase.domain.requirement.controller.dto.response.SourceResponse;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import com.skala.decase.domain.requirement.domain.Reception;
-import com.skala.decase.domain.requirement.domain.RequirementDocument;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.poi.ss.usermodel.BorderStyle;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.usermodel.FillPatternType;
-import org.apache.poi.ss.usermodel.Font;
-import org.apache.poi.ss.usermodel.HorizontalAlignment;
-import org.apache.poi.ss.usermodel.IndexedColors;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.VerticalAlignment;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
